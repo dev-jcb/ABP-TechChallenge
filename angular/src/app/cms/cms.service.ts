94bb4572fc4412d5e181e1f8ca05bcca49e2f4bd
JC Borlagdan <>
2025-04-23 05:11:58 -0400

    - fix edit row in cms component - implement get single content-entry

--- angular/src/app/cms/cms.service.ts
+++ angular/src/app/cms/cms.service.ts
@@ -1,48 +1,55 @@
 import { Injectable } from '@angular/core';
 
-import {RestService} from '@abp/ng.core';
+import { RestService } from '@abp/ng.core';
 import { PagedAndSortedResultRequestDto, PagedResultDto } from '@abp/ng.core';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class CmsService {
-  apiName = 'Default';
+    apiName = 'Default';
 
-  constructor(private restService: RestService) {}
+    constructor(private restService: RestService) { }
 
-  // Read: Get all content entries
-  getContentEntries = () =>
-    this.restService.request<any, PagedResultDto<any>>({
-        method: 'GET',
-        url: '/api/app/content-entry',
-        params: { sorting: 'creationTime desc', skipCount: 0, maxResultCount: 100 },
-      },
-        { apiName: this.apiName });
-
-  // Create: Add a new content entry
-  createContentEntry = (content:any) => 
-    this.restService.request<any, any>({
-        method: 'POST',
-        url: '/api/app/content-entry',
-        body: content,
+    // Read: Get all content entries
+    getContentEntries = () =>
+        this.restService.request<any, PagedResultDto<any>>({
+            method: 'GET',
+            url: '/api/app/content-entry',
+            params: { sorting: 'creationTime desc', skipCount: 0, maxResultCount: 100 },
         },
-        { apiName: this.apiName });
+            { apiName: this.apiName });
 
-  // Update: Edit an existing content entry
-  updateContentEntry = (content:any) =>
-    this.restService.request<any, any>({
-        method: 'PUT',
-        url: `/api/app/content-entry/${content.id}`,
-        body: content,
-      },
-      { apiName: this.apiName });
+    getContentEntry = (id: string) =>
+        this.restService.request<any, any>({
+            method: 'GET',
+            url: `/api/app/content-entry/${id}`,
+        },
+            { apiName: this.apiName });
 
-  // Delete: Remove a content entry
+    // Create: Add a new content entry
+    createContentEntry = (content: any) =>
+        this.restService.request<any, any>({
+            method: 'POST',
+            url: '/api/app/content-entry',
+            body: content,
+        },
+            { apiName: this.apiName });
+
+    // Update: Edit an existing content entry
+    updateContentEntry = (content: any) =>
+        this.restService.request<any, any>({
+            method: 'PUT',
+            url: `/api/app/content-entry/${content.id}`,
+            body: content,
+        },
+            { apiName: this.apiName });
+
+    // Delete: Remove a content entry
     deleteContentEntry = (id: number) =>
         this.restService.request<any, void>({
             method: 'DELETE',
             url: `/api/app/content-entry/${id}`,
         },
-        { apiName: this.apiName });
+            { apiName: this.apiName });
 }
